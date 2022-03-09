9f4db1525d96a96e7186a45d91807785136656fb
billijoe <yuxiankaiz@126.com>
2022-03-09 16:50:35 +0800

    flask-8081端口

---
+++ test_8081.py
@@ -0,0 +1,18 @@
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+        @Author yuxiankai
+        @Date 2022/3/9 4:47 下午
+        @Describe 
+        @Version 1.0
+    """
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World 8081'
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8081)
