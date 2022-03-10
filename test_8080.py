9bfd1e8991b58697c22c947e4d1d95aee8896849
billijoe <yuxiankaiz@126.com>
2022-03-10 16:00:34 +0800

    add methods=Get and Post

--- test_8080.py
+++ test_8080.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello_world():
     return 'Hello World 8080'
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080, debug=True)
